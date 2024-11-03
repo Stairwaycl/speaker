66ca2c53a5b988379c35f19e0ec78d21fa8c9020
equipo de Stairway <stairway.cl@gmail.com>
2024-11-03 17:39:35 -0300

    mejoras en el codigo speaker para detectar los nombres de los libros de forma automática

--- readme.md
+++ readme.md
@@ -1,20 +1,18 @@
 Esta es una app que transforma .pdf a .txt y puede leer .txt reproduciendolo en audio.
 
-Si se desea agregar nuevos libros, deben agregarse a la carpeta books, y dentro debe hacerse una carpeta con el nombre del autor (preferiblemente en minuscula y sin espacios), y dentro del nombre del autor, una carpeta con el nombre del libro con las mismas consideraciones que el autor, y dentro de ésta, carpetas, carpetas por capitulo, con el nombre "c + el numero del capítulo", y dentro de este, los archivos .txt por cada sección del capitulo con el nombre "s + numero del sección + nombre de seccion sin espacios". Del 1 al 9 en todos los casos, es preferible agregarle un 0 al principio, en especial si tiene mas de 9 secciones o más de 9 capítuos.
+Si se desea agregar nuevos libros, deben agregarse a la directorio books, y dentro una directorio con el nombre del libro con las mismas consideraciones que el autor, y dentro de ésta, directorios, directorios por capitulo, con el nombre "c + el numero del capítulo", y dentro de este, los archivos .txt por cada sección del capitulo con el nombre "s + numero del sección + nombre de seccion sin espacios". Del 1 al 9 en todos los casos, es preferible agregarle un 0 al principio, en especial si tiene mas de 9 secciones o más de 9 capítulos.
 
 Por supuesto, el código puede modificarse para que se ajuste a las necesidades en speaker.rb
 
 ---------------------------
 
-
-Al terminar, la idea es hacer un ejecutable .deb con ocra, ya que al hacerlo .deb, los usuarios no necesitaran instalar dependencias de forma manual
-
-además, la idea es que se pueda cargar cualquier archivo del escritorio, no solo el que está por defecto en la app.
-
-
 Mejoras:
+Hacer un ejecutable .deb con ocra, ya que al hacerlo .deb, los usuarios no necesitaran instalar dependencias de forma manual
 
-que calcule el tiempo que dura cada libro leido con el speaker, y que se pueda calcular cuanto se demoraria en leer el libro si uno lee tanto tiempo por día.
+Que se pueda cargar cualquier archivo del escritorio, no solo el que está por defecto en la app.
+que calcule el tiempo que dura cada libro leido con el speaker, y que se pueda calcular cuanto se demoraria en oir el libro si uno oye tanto tiempo o secciones por día.
 
-además, agregar un listado de preceptos que se encuentren en el libro en el jardin de la paz.
+Agregar un listado de preceptos que se encuentren en el libro en el jardin de la paz.
 agregar "ir a la sección anterior" o "cambiar de libro"
+
+Queremos agregar gem "threads" para poder ejecutar acciones mientras se está reproduciendo el audio.
--- speaker.rb
+++ speaker.rb
@@ -1,31 +1,31 @@
 require 'espeak'
 
 # Obtener los nombres de los directorios dentro de books
-books_dir = Dir.children('books').select { |d| d != '.' && d != '..' }
+books_dir = Dir.children('books').select { |d| d != '.' && d != '..' }.sort
 
-# Menú para selectionar el libro
+# Menú para selectionar el book
 puts "--------------------------------"
 puts "Selectione un libro para escuchar:"
-books_dir.sort.each_with_index do |libro, index|
-  puts "#{index + 1}. #{libro.capitalize}"
+books_dir.each_with_index do |book, index|
+  puts "#{index + 1}. #{book.capitalize}"
 end
 
 # Obtener la selección del usuario
 print "Ingrese el número de su selección: "
-selection = gets.chomp
+selection = gets.chomp.to_i
 
-def reproducir_texto(ruta_base, voice)
-  salir = false
+def play_text(ruta_base, voice)
+  go_to_menu = false
   dirs = Dir.children(ruta_base).select { |d| d.start_with?('c') }.sort
 
   dirs.each do |dir|
-    break if salir
-    ruta_libro = File.join(ruta_base, dir)
-    files = Dir.children(ruta_libro).select { |f| f.end_with?('.txt') }.sort
+    break if go_to_menu
+    ruta_book = File.join(ruta_base, dir)
+    files = Dir.children(ruta_book).select { |f| f.end_with?('.txt') }.sort
 
     files.each do |file|
-      break if salir
-      file_path = File.join(ruta_libro, file)
+      break if go_to_menu
+      file_path = File.join(ruta_book, file)
       text = File.read(file_path)
       speech = ESpeak::Speech.new(text,
         voice: voice,
@@ -37,10 +37,14 @@ def reproducir_texto(ruta_base, voice)
 
       loop do
         speech.speak
-        puts "Presione 'c' para continuar, 's' para salir o 'r' para reproducir nuevamente"
+        puts "Ingrese"
+        puts "'c' para continuar"
+        puts "'s' para ir al menú principal"
+        puts "'r' para reproducir nuevamente"
+        print ">"
         input = gets.chomp
         if input.downcase == 's'
-          salir = true
+          go_to_menu = true
           break
         elsif input.downcase == 'r'
           next
@@ -50,22 +54,11 @@ def reproducir_texto(ruta_base, voice)
       end
     end
   end
-  exit if salir
+  exit if go_to_menu
 end
 
-case selection
-when "1"
-  ruta_base = "books/en-el-jardin-de-la-paz-solo-para-hombres"
-  reproducir_texto(ruta_base, "es")
+# Obtener el nombre del libro seleccionado
+libro_seleccionado = books_dir[selection - 1]
+ruta_base = "books/#{libro_seleccionado}"
 
-when "2"
-  ruta_base = "books/bereshit"
-  reproducir_texto(ruta_base, "en")
-
-when "3"
-  ruta_base = "books/kitzur-shuljan-arusj"
-  reproducir_texto(ruta_base, "es")
-
-else
-  puts "Selección inválida"
-end
+play_text(ruta_base, "es")
